9ceed6b4955caeec6e362d74acfa1e3f14be8c89
Ryan Hansohn <hansohn@users.noreply.github.com>
2018-06-17 23:50:49 -0700

    seperate out cluster file placement

--- recipes/hw_cluster.rb
+++ recipes/hw_cluster.rb
@@ -12,24 +12,6 @@ directory 'make_/var/lib/ambari-clusters' do
   group 'root'
 end
 
-# hw-cluster: add blueprint to cluster dir
-template "create_/var/lib/ambari-clusters/#{node['hw']['cluster']['blueprint_file']}" do
-  path "/var/lib/ambari-clusters/#{node['hw']['cluster']['blueprint_file']}"
-  source "#{node['hw']['cluster']['blueprint_file']}.erb"
-  sensitive true
-  owner node['hw']['ambari']['server']['user']['name']
-  group 'root'
-end
-
-# hw-cluster: add hostmapping to cluster dir
-template "create_/var/lib/ambari-clusters/#{node['hw']['cluster']['hostmapping_file']}" do
-  path "/var/lib/ambari-clusters/#{node['hw']['cluster']['hostmapping_file']}"
-  source "#{node['hw']['cluster']['hostmapping_file']}.erb"
-  sensitive true
-  owner node['hw']['ambari']['server']['user']['name']
-  group 'root'
-end
-
 # set ambari_server var based on Chef Search
 # set to hostname for Chef-Zero and Chef-Solo
 ambari_server = if Chef::Config[:local_mode]
