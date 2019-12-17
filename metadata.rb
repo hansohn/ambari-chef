ec8e2761a2827d0d30795600653e3b02e2f4cc54
Ryan Hansohn <hansohn@users.noreply.github.com>
2019-12-17 15:24:02 -0800

    bump version

--- metadata.rb
+++ metadata.rb
@@ -3,9 +3,8 @@ maintainer 'Ryan Hansohn'
 maintainer_email 'info@imnorobot.com'
 license 'MIT'
 description 'Installs/Configures ambari-chef'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '1.0.2'
-chef_version '>= 12.14' if respond_to?(:chef_version)
+version '1.1.0'
+chef_version '>= 12.14'
 
 supports 'centos', '>= 7.0'
 
@@ -13,5 +12,5 @@ depends 'hostsfile'
 depends 'java-chef'
 depends 'ulimit'
 
-source_url 'https://github.com/hansohn/ambari-chef' if respond_to?(:source_url)
-issues_url 'https://github.com/hansohn/ambari-chef/issues' if respond_to?(:issues_url)
+source_url 'https://github.com/hansohn/ambari-chef'
+issues_url 'https://github.com/hansohn/ambari-chef/issues'
